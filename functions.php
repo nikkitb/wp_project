9599ad41e1b5057bf55d85135ef629ee1c7851aa
nik.kit.b <nikita.bashurov.92@gmail.com>
2023-02-27 22:38:01 +0100

    feat: add links

--- functions.php
+++ functions.php
@@ -3,5 +3,7 @@
 
     function childhood_scripts(){
         wp_enqueue_style('childhood-style', get_stylesheet_uri());
+        wp_enqueue_script('childhood-style', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
     }
 ?>
+
