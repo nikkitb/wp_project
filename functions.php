f8715febf60b95aae313c3e5bcb17535f442ac6b
nik.kit.b <nikita.bashurov.92@gmail.com>
2023-03-04 23:53:05 +0100

    feat: add links

--- functions.php
+++ functions.php
@@ -5,5 +5,8 @@
         wp_enqueue_style('childhood-style', get_stylesheet_uri());
         wp_enqueue_script('childhood-style', get_template_directory_uri() . '/assets/js/main.min.js', array(), null, true);
     }
+
+
+    add_theme_support('custom-logo');
 ?>
 
